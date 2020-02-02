a8127a6745661426cac5dcb9a6371841aa63830f
pepuuu-gaming <tgpatrick26@gmail.com>
2020-02-02 19:01:41 +0800

    Counting 0 to 9 when start button is pressed

---
+++ Matrix(5x5)/Form1.vb
@@ -0,0 +1,356 @@
+﻿Public Class Form1
+
+    Dim n As SByte = 0
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.BackColor = Color.Gray
+        Label2.BackColor = Color.Gray
+        Label3.BackColor = Color.Gray
+        Label4.BackColor = Color.Gray
+        Label5.BackColor = Color.Gray
+
+        Label6.BackColor = Color.Gray
+        Label7.BackColor = Color.Gray
+        Label8.BackColor = Color.Gray
+        Label9.BackColor = Color.Gray
+        Label10.BackColor = Color.Gray
+
+        Label11.BackColor = Color.Gray
+        Label12.BackColor = Color.Gray
+        Label13.BackColor = Color.Gray
+        Label14.BackColor = Color.Gray
+        Label15.BackColor = Color.Gray
+
+        Label16.BackColor = Color.Gray
+        Label17.BackColor = Color.Gray
+        Label18.BackColor = Color.Gray
+        Label19.BackColor = Color.Gray
+        Label20.BackColor = Color.Gray
+
+        Label21.BackColor = Color.Gray
+        Label22.BackColor = Color.Gray
+        Label23.BackColor = Color.Gray
+        Label24.BackColor = Color.Gray
+        Label25.BackColor = Color.Gray
+
+        If n = 0 Then
+            Label1.BackColor = Color.Green
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Green
+            Label5.BackColor = Color.Green
+
+            Label6.BackColor = Color.Green
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Gray
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Green
+
+            Label11.BackColor = Color.Green
+            Label12.BackColor = Color.Gray
+            Label13.BackColor = Color.Gray
+            Label14.BackColor = Color.Gray
+            Label15.BackColor = Color.Green
+
+            Label16.BackColor = Color.Green
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Gray
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Green
+
+            Label21.BackColor = Color.Green
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Green
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Green
+
+        ElseIf n = 1 Then
+            Label1.BackColor = Color.Gray
+            Label2.BackColor = Color.Gray
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Gray
+            Label5.BackColor = Color.Gray
+
+            Label6.BackColor = Color.Gray
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Green
+            Label9.BackColor = Color.Green
+            Label10.BackColor = Color.Gray
+
+            Label11.BackColor = Color.Gray
+            Label12.BackColor = Color.Gray
+            Label13.BackColor = Color.Green
+            Label14.BackColor = Color.Gray
+            Label15.BackColor = Color.Green
+
+            Label16.BackColor = Color.Gray
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Green
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Gray
+
+            Label21.BackColor = Color.Green
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Green
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Green
+
+        ElseIf n = 2 Then
+            Label1.BackColor = Color.Green
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Green
+            Label5.BackColor = Color.Green
+
+            Label6.BackColor = Color.Green
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Gray
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Gray
+
+            Label11.BackColor = Color.Green
+            Label12.BackColor = Color.Green
+            Label13.BackColor = Color.Green
+            Label14.BackColor = Color.Green
+            Label15.BackColor = Color.Green
+
+            Label16.BackColor = Color.Gray
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Gray
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Green
+
+            Label21.BackColor = Color.Green
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Green
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Green
+
+        ElseIf n = 3 Then
+            Label1.BackColor = Color.Green
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Green
+            Label5.BackColor = Color.Green
+
+            Label6.BackColor = Color.Green
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Gray
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Gray
+
+            Label11.BackColor = Color.Green
+            Label12.BackColor = Color.Green
+            Label13.BackColor = Color.Green
+            Label14.BackColor = Color.Green
+            Label15.BackColor = Color.Green
+
+            Label16.BackColor = Color.Green
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Gray
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Gray
+
+            Label21.BackColor = Color.Green
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Green
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Green
+
+        ElseIf n = 4 Then
+            Label1.BackColor = Color.Gray
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Gray
+            Label4.BackColor = Color.Gray
+            Label5.BackColor = Color.Gray
+
+            Label6.BackColor = Color.Gray
+            Label7.BackColor = Color.Green
+            Label8.BackColor = Color.Green
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Gray
+
+            Label11.BackColor = Color.Gray
+            Label12.BackColor = Color.Green
+            Label13.BackColor = Color.Gray
+            Label14.BackColor = Color.Green
+            Label15.BackColor = Color.Gray
+
+            Label16.BackColor = Color.Green
+            Label17.BackColor = Color.Green
+            Label18.BackColor = Color.Green
+            Label19.BackColor = Color.Green
+            Label20.BackColor = Color.Green
+
+            Label21.BackColor = Color.Gray
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Gray
+            Label24.BackColor = Color.Gray
+            Label25.BackColor = Color.Gray
+
+        ElseIf n = 5 Then
+            Label1.BackColor = Color.Green
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Green
+            Label5.BackColor = Color.Green
+
+            Label6.BackColor = Color.Gray
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Gray
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Green
+
+            Label11.BackColor = Color.Green
+            Label12.BackColor = Color.Green
+            Label13.BackColor = Color.Green
+            Label14.BackColor = Color.Green
+            Label15.BackColor = Color.Green
+
+            Label16.BackColor = Color.Green
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Gray
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Gray
+
+            Label21.BackColor = Color.Green
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Green
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Green
+
+        ElseIf n = 6 Then
+            Label1.BackColor = Color.Green
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Green
+            Label5.BackColor = Color.Green
+
+            Label6.BackColor = Color.Gray
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Gray
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Green
+
+            Label11.BackColor = Color.Green
+            Label12.BackColor = Color.Green
+            Label13.BackColor = Color.Green
+            Label14.BackColor = Color.Green
+            Label15.BackColor = Color.Green
+
+            Label16.BackColor = Color.Green
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Gray
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Green
+
+            Label21.BackColor = Color.Green
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Green
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Green
+
+        ElseIf n = 7 Then
+            Label1.BackColor = Color.Green
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Green
+            Label5.BackColor = Color.Green
+
+            Label6.BackColor = Color.Green
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Gray
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Gray
+
+            Label11.BackColor = Color.Gray
+            Label12.BackColor = Color.Green
+            Label13.BackColor = Color.Green
+            Label14.BackColor = Color.Green
+            Label15.BackColor = Color.Gray
+
+            Label16.BackColor = Color.Gray
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Green
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Gray
+
+            Label21.BackColor = Color.Gray
+            Label22.BackColor = Color.Gray
+            Label23.BackColor = Color.Gray
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Gray
+
+        ElseIf n = 8 Then
+            Label1.BackColor = Color.Green
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Green
+            Label5.BackColor = Color.Green
+
+            Label6.BackColor = Color.Green
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Gray
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Green
+
+            Label11.BackColor = Color.Green
+            Label12.BackColor = Color.Green
+            Label13.BackColor = Color.Green
+            Label14.BackColor = Color.Green
+            Label15.BackColor = Color.Green
+
+            Label16.BackColor = Color.Green
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Gray
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Green
+
+            Label21.BackColor = Color.Green
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Green
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Green
+
+        ElseIf n = 9 Then
+            Label1.BackColor = Color.Green
+            Label2.BackColor = Color.Green
+            Label3.BackColor = Color.Green
+            Label4.BackColor = Color.Green
+            Label5.BackColor = Color.Green
+
+            Label6.BackColor = Color.Green
+            Label7.BackColor = Color.Gray
+            Label8.BackColor = Color.Gray
+            Label9.BackColor = Color.Gray
+            Label10.BackColor = Color.Green
+
+            Label11.BackColor = Color.Green
+            Label12.BackColor = Color.Green
+            Label13.BackColor = Color.Green
+            Label14.BackColor = Color.Green
+            Label15.BackColor = Color.Green
+
+            Label16.BackColor = Color.Green
+            Label17.BackColor = Color.Gray
+            Label18.BackColor = Color.Gray
+            Label19.BackColor = Color.Gray
+            Label20.BackColor = Color.Gray
+
+            Label21.BackColor = Color.Green
+            Label22.BackColor = Color.Green
+            Label23.BackColor = Color.Green
+            Label24.BackColor = Color.Green
+            Label25.BackColor = Color.Green
+
+        Else
+            Timer1.Stop()
+            n = -1
+        End If
+        n += 1
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+End Class
